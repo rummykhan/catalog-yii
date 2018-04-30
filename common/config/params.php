446102f5a2b2bd137cf0697ee9e5e150b641e9fd
Rehan Manzoor <rehan_manzoor@outlook.com>
2018-04-30 18:56:10 +0400

    Added googlemaps key

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    'googleMapsKey' => 'AIzaSyD3BJEtDrT33i29g88As02p0eqU-SDExCI',
 ];
