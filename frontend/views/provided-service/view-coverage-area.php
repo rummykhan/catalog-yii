70919313be2e0bcef6534a4f8d63b375a18d2d88
Rehan Manzoor <rehan_manzoor@outlook.com>
2018-04-30 18:54:23 +0400

    Added the coverage area

--- frontend/views/provided-service/view-coverage-area.php
+++ frontend/views/provided-service/view-coverage-area.php
@@ -25,6 +25,9 @@ $service = $model->service;
 
 ?>
 
+<a href="<?= Url::to(['/provided-service/add-type', 'id' => $model->id]) ?>" class="btn btn-primary">Add Service Type</a>
+<a href="<?= Url::to(['/provided-service/add-coverage-area', 'id' => $model->id]) ?>" class="btn btn-primary">Add Coverage Area</a>
+<br><br>
 
 <?= GridView::widget([
     'dataProvider' => $dataProvider,
