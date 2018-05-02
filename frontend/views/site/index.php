61a45441bea1a316c171b157b8778ebbfaf0f101
Rehan Manzoor <rehan_manzoor@outlook.com>
2018-05-02 16:58:08 +0400

    added the home page

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -4,3 +4,20 @@
 
 $this->title = 'Service Catalog';
 ?>
+
+<br><br><br>
+
+<div class="row text-center">
+    <div class="col-md-4 well">
+        <a href="/category/index">Categories</a>
+    </div>
+
+    <div class="col-md-4 well">
+        <a href="/service/index">Services</a>
+    </div>
+
+    <div class="col-md-4 well">
+        <a href="/provider/index">Providers</a>
+    </div>
+
+</div>
